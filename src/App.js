694872c989d24451f9ef3faef60e43ff6df5bed0
mhossei4 <milad.hosseini@coles.com.au>
2022-03-15 12:13:58 +1100

    Added dropdowns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState , useCallback} from 'react';
 import { Box, Button, Input, Flex, LoadingIndicator } from 'roo-ui/components';
 import styled from '@emotion/styled';
 
-const MASTER_CARD_SESSION_JS_SRC = `https://test-gateway.mastercard.com/form/version/52/merchant/${process.env.REACT_APP_MERCHANT_ID}/session.js`;
+const MERCHANT_ID = '';// use your merchant ID here
+const MASTER_CARD_SESSION_JS_SRC = `https://test-cipg.mtf.gateway.mastercard.com/form/version/56/merchant/${MERCHANT_ID}/session.js`;
 const MPGS_TIMEOUT = 5000;
 
 const onScriptLoad = ({
@@ -64,7 +65,7 @@ const loadScript = async (formSessionUpdate) => {
     if (prevScript) {
       prevScript.remove();
     }
-  
+
     const script = document.createElement('script');
     script.src = MASTER_CARD_SESSION_JS_SRC;
     script.async = 1;
@@ -73,9 +74,9 @@ const loadScript = async (formSessionUpdate) => {
       initialized: resolve,
       formSessionUpdate,
     });
-  
+
     document.body.appendChild(script);
-  }); 
+  });
 }
 
 const Wrapper = styled(Box)`
@@ -141,20 +142,42 @@ const Payment = ({
         <Flex position="absolute" top="0" right="0" bottom="0" left="0" bg="rgba(0, 0, 0, 0.1)" alignItems="center">
           <LoadingIndicator />
         </Flex>
-      )} 
+      )}
       <Box my="2">Please enter your payment details:</Box>
       <h3>Credit Card</h3>
       <Box my="2">
-        Card Number: 
+        Card Number:
         <Input type="text" mb="0" id="card-number" title="card number" aria-label="enter your card number" readOnly />
       </Box>
       <Box my="2">
         Expiry Month:
-        <Input type="text" mb="0" id="expiry-month" title="expiry month" aria-label="two digit expiry month" readOnly />
+        <select type="text" mb="0" id="expiry-month" title="expiry year" aria-label="two digit expiry year" readOnly >
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">Jun</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">November</option>
+          <option value="11">October</option>
+          <option value="12">December</option>
+        </select>
       </Box>
       <Box my="2">
         Expiry Year:
-        <Input type="text" mb="0" id="expiry-year" title="expiry year" aria-label="two digit expiry year" readOnly />
+        <select type="text" mb="0" id="expiry-year" title="expiry year" aria-label="two digit expiry year" readOnly >
+          <option value="2025">2025</option>
+          <option value="2026">2026</option>
+          <option value="2027">2027</option>
+          <option value="2028">2028</option>
+          <option value="2029">2029</option>
+          <option value="2030">2030</option>
+          <option value="2031">2031</option>
+          <option value="2033">2033</option>
+        </select>
       </Box>
       <Box my="2">
         Security Code:
@@ -175,7 +198,7 @@ const Payment = ({
 
 const App = () => {
   const [toggle, setToggle] = useState(false);
-  
+
   return (
     <Box p="4">
       {toggle && (<Payment onFormSessionUpdated={alert} />)}
